5d43239a753d33d9a0d521f30ef089f68aff384f
Kakumanu-Harshitha <harshithakakumanu2006@gmail.com>
2025-09-01 18:04:03 +0530

    new code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="Harshitha  Kakumanu ",
+    author_email="harshithakakumanu2006@gmail.com",
+    packages=find_packages(),
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+)
